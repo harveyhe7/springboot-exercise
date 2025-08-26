fda1b60f4818b467000bf7cbf53614c1a9e149e9
harveyhe7 <harvey.he@oocl.com>
2025-08-26 22:14:25 +0800

    feat: 添加companies通过id的get请求

--- src/main/java/com/oocl/training/controller/CompanyController.java
+++ src/main/java/com/oocl/training/controller/CompanyController.java
@@ -1,9 +1,6 @@
 package com.oocl.training.controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -39,5 +36,12 @@ public class CompanyController {
     }
 
 
+    @GetMapping("/{id}")
+    public Company getCompanyById(@PathVariable int id) {
+        return companyList.stream()
+                .filter(c -> c.getId() == id)
+                .findFirst()
+                .orElse(null);
+    }
 
 }
