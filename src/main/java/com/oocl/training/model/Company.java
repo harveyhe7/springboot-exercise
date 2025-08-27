41b186da2ee24af5e4210eb834dd1a2286a855a9
Harvey He <HEHA7@oocl.com>
2025-08-27 14:11:07 +0800

    refactor: 两个实体类增加无参构造函数，修改Company部分参数的构造函数

--- src/main/java/com/oocl/training/model/Company.java
+++ src/main/java/com/oocl/training/model/Company.java
@@ -3,15 +3,19 @@ package com.oocl.training.model;
 import com.oocl.training.model.Employee;
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.util.List;
 @Data
 @AllArgsConstructor
+@NoArgsConstructor
 public class Company {
     int id;
     String name;
     List<Employee> employees;
 
-    public Company(Integer id, String global_innovators) {
+    public Company(Integer id, String name) {
+        this.id = id;
+        this.name = name;
     }
 }
--- src/main/java/com/oocl/training/model/Employee.java
+++ src/main/java/com/oocl/training/model/Employee.java
@@ -3,9 +3,11 @@ package com.oocl.training.model;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
 @AllArgsConstructor
+@NoArgsConstructor
 public class Employee {
     int id;
     String name;
