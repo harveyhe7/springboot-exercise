44db1d6923c537e8c1c08ea4469ff69147e3edf5
Harvey He <HEHA7@oocl.com>
2025-08-28 18:02:29 +0800

    feat：增加资源未找到异常处理类

---
+++ src/main/java/com/oocl/training/exception/ResourceNotFoundException.java
@@ -0,0 +1,7 @@
+package com.oocl.training.exception;
+
+public class ResourceNotFoundException extends RuntimeException{
+    public ResourceNotFoundException(String message) {
+        super(message);
+    }
+}
