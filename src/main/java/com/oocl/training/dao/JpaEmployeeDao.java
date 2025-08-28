5ea6b72ccdae248e474ef193c073f12a5e35781f
Harvey He <HEHA7@oocl.com>
2025-08-28 18:03:36 +0800

    feat：增加jpa接口实现一些方法

---
+++ src/main/java/com/oocl/training/dao/JpaEmployeeDao.java
@@ -0,0 +1,12 @@
+package com.oocl.training.dao;
+
+import com.oocl.training.model.Employee;
+import jakarta.persistence.Id;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface JpaEmployeeDao extends JpaRepository<Employee, Integer> {
+    List<Employee> getEmployeeByGender(String gender);
+    Employee findByCompanyId(int companyId);
+}
