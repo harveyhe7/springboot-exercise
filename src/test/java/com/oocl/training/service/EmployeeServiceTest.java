3f8569420af3c5de8d5f1986222d21f78c13550f
Harvey He <HEHA7@oocl.com>
2025-08-28 18:00:21 +0800

    test:增加Employee的集成测试类

--- src/test/java/com/oocl/training/service/EmployeeServiceTest.java
+++ src/test/java/com/oocl/training/service/EmployeeServiceTest.java
@@ -1,6 +1,8 @@
 package com.oocl.training.service;
 
 import com.oocl.training.dao.EmployeeDao;
+import com.oocl.training.dao.EmployeeDbDao;
+import com.oocl.training.dao.EmployeeMemoryDao;
 import com.oocl.training.exception.InvailEmployeeException;
 import com.oocl.training.model.Employee;
 import org.junit.jupiter.api.Test;
@@ -23,7 +25,7 @@ class EmployeeServiceTest {
 
 
     @Mock
-    private EmployeeDao employeeDao;
+    private EmployeeDbDao employeeDbDao;
 
     @InjectMocks
     private EmployeeService employeeService;
@@ -32,8 +34,8 @@ class EmployeeServiceTest {
     void shuold_create_employee_successfully() {
 //        given
         Employee inputEmployee = new Employee("oocl", 25, "Male", 8000, true);
-        Employee dbOutMockEmployee = new Employee(6, inputEmployee.getName(), inputEmployee.getAge(), inputEmployee.getGender(), inputEmployee.getSalary(), true);
-        Mockito.when(employeeDao.save(Mockito.any(Employee.class))).thenReturn(dbOutMockEmployee);
+        Employee dbOutMockEmployee = new Employee(6, inputEmployee.getName(), inputEmployee.getAge(), inputEmployee.getGender(), inputEmployee.getSalary(), true,1);
+        Mockito.when(employeeDbDao.save(Mockito.any(Employee.class))).thenReturn(dbOutMockEmployee);
 //        when
         Employee savedEmployee = employeeService.createEmployee(inputEmployee);
 //        then
@@ -72,30 +74,30 @@ class EmployeeServiceTest {
 //        given
         Integer id = 1;
         Employee inputEmployee = new Employee("oocl", 20, "Male", 8000, true);
-        Employee dbOutMockEmployee = new Employee(id, inputEmployee.getName(), inputEmployee.getAge() + 1, inputEmployee.getGender(), inputEmployee.getSalary() + 1000, true);
-        Mockito.when(employeeDao.getEmployeeById(Mockito.any(Integer.class))).thenReturn(inputEmployee);
-        Mockito.when(employeeDao.updateEmployee(Mockito.any(Integer.class), Mockito.any(Integer.class), Mockito.any(Integer.class))).thenReturn(dbOutMockEmployee);
+        Employee dbOutMockEmployee = new Employee(id, inputEmployee.getName(), inputEmployee.getAge() + 1, inputEmployee.getGender(), inputEmployee.getSalary() + 1000, true,1);
+        Mockito.when(employeeDbDao.getEmployeeById(Mockito.any(Integer.class))).thenReturn(inputEmployee);
+        Mockito.when(employeeDbDao.updateEmployee(Mockito.any(Integer.class), Mockito.any(Integer.class), Mockito.any(Integer.class))).thenReturn(dbOutMockEmployee);
 //        when
         Employee updatedEmployee = employeeService.updateEmployee(id);
 //        then
         assertEquals(inputEmployee.getAge() + 1, updatedEmployee.getAge());
         assertEquals(inputEmployee.getSalary() + 1000, updatedEmployee.getSalary());
-        verify(employeeDao, times(1)).updateEmployee(id, 1000, 1);
-        verify(employeeDao, times(1)).getEmployeeById(id);
+        verify(employeeDbDao, times(1)).updateEmployee(id, 1000, 1);
+        verify(employeeDbDao, times(1)).getEmployeeById(id);
     }
 
     @Test
     void get_employees_by_id_successfully() {
 //        given
         int id = 1;
-        Employee dbOutMockEmployee = new Employee(1, "oocl", 20, "Male", 8000, true);
-        Mockito.when(employeeDao.getEmployeeById(Mockito.any(Integer.class))).thenReturn(dbOutMockEmployee);
+        Employee dbOutMockEmployee = new Employee(1, "oocl", 20, "Male", 8000, true,1);
+        Mockito.when(employeeDbDao.getEmployeeById(Mockito.any(Integer.class))).thenReturn(dbOutMockEmployee);
 //        when
         Employee employee = employeeService.getEmployeeById(id);
 //        then
         assertEquals(dbOutMockEmployee.getName(), employee.getName());
         assertEquals(dbOutMockEmployee.getAge(), employee.getAge());
-        verify(employeeDao, times(1)).getEmployeeById(id);
+        verify(employeeDbDao, times(1)).getEmployeeById(id);
     }
 
     @Test
@@ -105,27 +107,27 @@ class EmployeeServiceTest {
 //        when
         employeeService.removeEmployee(id);
 //        then
-        verify(employeeDao, times(1)).remove(id);
+        verify(employeeDbDao, times(1)).remove(id);
     }
 
     @Test
     void get_all_employees_successfully() {
 //        given
         Map<Integer, Employee> employees = new HashMap<>(Map.of(
-                1, new Employee(1, "John Smith", 32, "Male", 5000.0, true),
-                2, new Employee(2, "Jane Johnson", 28, "Female", 6000.0, true),
-                3, new Employee(3, "David Williams", 35, "Male", 5500.0, true),
-                4, new Employee(4, "Emily Brown", 23, "Female", 4500.0, true),
-                5, new Employee(5, "Michael Jones", 40, "Male", 7000.0, true)));
+                1, new Employee(1, "John Smith", 32, "Male", 5000.0, true,1),
+                2, new Employee(2, "Jane Johnson", 28, "Female", 6000.0, true,1),
+                3, new Employee(3, "David Williams", 35, "Male", 5500.0, true,1),
+                4, new Employee(4, "Emily Brown", 23, "Female", 4500.0, true,1),
+                5, new Employee(5, "Michael Jones", 40, "Male", 7000.0, true,1)));
 
         ArrayList<Employee> dbOutMockEmployees = new ArrayList<>(employees.values());
 
-        Mockito.when(employeeDao.getAllEmployees()).thenReturn(dbOutMockEmployees);
+        Mockito.when(employeeDbDao.getAllEmployees()).thenReturn(dbOutMockEmployees);
 //        when
         ArrayList<Employee> resultEmployee =  employeeService.getAllEmployees(null);
 //        then
-        verify(employeeDao, times(1)).getAllEmployees();
-        verify(employeeDao, times(0)).getGenderEmployees(Mockito.any(String.class));
+        verify(employeeDbDao, times(1)).getAllEmployees();
+        verify(employeeDbDao, times(0)).getEmployeesByGender(Mockito.any(String.class));
         assertEquals(5, resultEmployee.size());
     }
 
@@ -134,22 +136,22 @@ class EmployeeServiceTest {
 //        given
         String gender = "Male";
         Map<Integer, Employee> employees = new HashMap<>(Map.of(
-                1, new Employee(1, "John Smith", 32, "Male", 5000.0, true),
-                2, new Employee(2, "Jane Johnson", 28, "Female", 6000.0, true),
-                3, new Employee(3, "David Williams", 35, "Male", 5500.0, true),
-                4, new Employee(4, "Emily Brown", 23, "Female", 4500.0, true),
-                5, new Employee(5, "Michael Jones", 40, "Male", 7000.0, true)));
+                1, new Employee(1, "John Smith", 32, "Male", 5000.0, true,1),
+                2, new Employee(2, "Jane Johnson", 28, "Female", 6000.0, true,1),
+                3, new Employee(3, "David Williams", 35, "Male", 5500.0, true,1),
+                4, new Employee(4, "Emily Brown", 23, "Female", 4500.0, true,1),
+                5, new Employee(5, "Michael Jones", 40, "Male", 7000.0, true,1)));
         employees.remove(2);
         employees.remove(4);
         ArrayList<Employee> dbOutMockEmployees = new ArrayList<>(employees.values());
-        Mockito.when(employeeDao.getGenderEmployees(gender)).thenReturn(dbOutMockEmployees);
+        Mockito.when(employeeDbDao.getEmployeesByGender(gender)).thenReturn(dbOutMockEmployees);
 
 //        when
-        ArrayList<Employee> resultEmployee = employeeDao.getGenderEmployees(gender);
+        ArrayList<Employee> resultEmployee = employeeDbDao.getEmployeesByGender(gender);
 //        then
         assertEquals(3, resultEmployee.size());
         assertEquals("Male", resultEmployee.get(0).getGender());
-        verify(employeeDao, times(1)).getGenderEmployees(Mockito.any(String.class));
+        verify(employeeDbDao, times(1)).getEmployeesByGender(Mockito.any(String.class));
 
     }
 }
